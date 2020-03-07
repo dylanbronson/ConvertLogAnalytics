a7262e0f69d1c6f55040ccc9647fb5a2a376d1ce
dylanbronson <55515325+dylanbronson@users.noreply.github.com>
2020-03-06 17:09:26 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace ConvertLogAnalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            string text = System.IO.File.ReadAllText(args[0]);
+            Console.WriteLine("Got text");
+            dynamic stuff = JsonConvert.DeserializeObject(text);
+            Console.WriteLine("Got JSON. Going through now...");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(args[1]))
+            {
+                foreach (var s in stuff)
+                {
+                    file.WriteLine(s.timestamp + " " + s.text);
+                }
+            }
+        }
+    }
+}
